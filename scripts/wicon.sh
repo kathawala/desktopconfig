1fc8b100813efa429f0277db6aefe733d599cd20
Farhan Kathawala <farhank@stanford.edu>
2014-11-09 21:30:07 -0800

    Update wicon.sh

--- scripts/wicon.sh
+++ scripts/wicon.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Parses the weatherunderground xml file for a weather icon to go
+# with the weather forecast
+
 ICON=$(grep -oPm1 "(?<=<icon_url>)[^<]+" ~/bin/w.xml)
 
 curl -s $ICON > ~/bin/w.gif
